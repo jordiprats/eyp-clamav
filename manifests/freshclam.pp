2781b95631638ec8b7e462c1169ae1120ed4eb82
Jordi Prats <jprats@systemadmin.es>
2016-11-19 17:47:40 +0100

    conf freschclam

--- manifests/freshclam.pp
+++ manifests/freshclam.pp
@@ -1,17 +1,13 @@
 class clamav::freshclam(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits clamav::params{
+                          $manage_package = true,
+                          $package_ensure = 'installed',
+                          $mailto         = undef,
+                        ) inherits clamav::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
   class { '::clamav::freshclam::install': } ->
-  class { '::clamav::freshclam::config': } ~>
-  class { '::clamav::freshclam::service': } ->
+  class { '::clamav::freshclam::config': } ->
   Class['::clamav::freshclam']
 
 }
--- manifests/freshclam/config.pp
+++ manifests/freshclam/config.pp
@@ -1,3 +1,19 @@
 class clamav::freshclam::config inherits clamav::freshclam {
 
+  file { '/etc/cron.d/clamav-update':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => template("${module_name}/freshclam/cronjob.erb"),
+  }
+
+  file { '/etc/freshclam.conf':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/freshclam/freshclamconf.erb"),
+  }
+
 }
