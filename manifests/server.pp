01a693c9268d0caeeb27f1bd0d547f1f01592cf5
Jordi Prats <jprats@systemadmin.es>
2016-11-19 17:30:08 +0100

    fora relació alre class

--- manifests/server.pp
+++ manifests/server.pp
@@ -9,7 +9,6 @@ class clamav::server(
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
-  Class['::clamav'] ->
   class { '::clamav::server::install': } ->
   class { '::clamav::server::config': } ~>
   class { '::clamav::server::service': } ->
