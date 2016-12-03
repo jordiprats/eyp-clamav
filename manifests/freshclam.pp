b6c42b59e51aba3af45ca11aa31fba2de7caf4c1
Jordi Prats <jprats@systemadmin.es>
2016-12-03 09:55:05 +0100

    afegida variable maxattempts

--- manifests/freshclam.pp
+++ manifests/freshclam.pp
@@ -2,6 +2,7 @@ class clamav::freshclam(
                           $manage_package = true,
                           $package_ensure = 'installed',
                           $mailto         = undef,
+                          $maxattempts    = '5',
                         ) inherits clamav::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
